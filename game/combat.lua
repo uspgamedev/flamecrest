c90429d4a87e910615b036b9041983b71eb0e8f0
Renan <renanteruoc@gmail.com>
2012-06-29 18:46:34 -0300

    simple combat and units implemented with löve

---
+++ game/combat.lua
@@ -0,0 +1,47 @@
+function combat (unit1, unit2)
+  math.randomseed( os.time() ) -- Retirar essa coisa aqui depois
+  strike(unit1, unit2)
+  if (unit2:isdead()) then
+    return
+  end
+  strike(unit2, unit1)
+  if (unit1:isdead()) then
+    return
+  end
+  faster, slower = muchfaster(unit1, unit2)
+  if (faster) then
+    print("moreattack")
+    strike(faster, slower)
+  end
+end
+
+function muchfaster (unit1, unit2)
+  if (unit1.spd -4 >= unit2.spd) then
+    return unit1, unit2
+  end
+  if (unit2.spd -4 >= unit1.spd)  then
+    return unit2, unit1
+  end
+  return false, false
+end
+
+function strike (unit1, unit2)
+  hitbonus = 80
+  hit = 2 * unit1.skl + unit1.lck + hitbonus
+  evade = 2 * unit2.spd + unit2.lck
+  hitchance = hit - evade
+  damage = unit1.str - unit2.def
+  rand1 = math.random(100)
+  rand2 = math.random(100)
+  if ((rand1 + rand2) / 2 <= hitchance) then --Double RNG as seen in the games!
+    crit = unit1.skl / 2
+    dodge = unit2.lck
+    critchance = crit - dodge
+    rand1 = math.random(100)
+    if (rand1 <= critchance) then
+      print("crits")
+      damage = damage * 3
+    end
+    unit2:takedamage(damage)
+  end
+end
---
+++ game/unit.lua
@@ -0,0 +1,24 @@
+unit = {
+  name = "Soldier",
+  str = 10,
+  def = 10,
+  spd = 10,
+  skl = 10,
+  lck = 10,
+  hp = 16
+}
+
+function unit:new (newguy)
+  newguy = newguy or {}
+  self.__index = self --da fuq
+  setmetatable(newguy, self) --da fuq 2
+  return newguy
+end
+
+function unit:takedamage (dmg)
+  self.hp = self.hp - dmg
+end
+
+function unit:isdead ()
+  return self.hp <= 0
+end
