671d0ce013bef375c19ccee669ee51ea3033929a
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2012-07-23 19:06:15 -0300

    FIGHT buttons.

--- game/layout.lua
+++ game/layout.lua
@@ -1,6 +1,7 @@
 
 require "nova.table"
 require "button"
+require "game"
 
 module ("layout", package.seeall) do
 
@@ -10,11 +11,14 @@ module ("layout", package.seeall) do
 
   function draw (g)
 
+    -- draw lines of basic layout
     g.setColor { 50, 50, 50, 255 }
     g.line(32, 64, 1024-32, 64)
     g.line(512, 64, 512, 768-16)
+    g.line(32, 768/2, 1024-32, 768/2)
     g.setColor { 255, 255, 255, 255 }
 
+    -- draw buttons
     for _,v in pairs(buttons) do
       v:draw()
     end
@@ -36,7 +40,20 @@ module ("layout", package.seeall) do
       text = "RESET ALL",
       action = function () end
     },
-    --
+    -- unit1 attacks unit2
+    button:new {
+      pos = vec2:new { 512-16-128, 16 },
+      size = vec2:new { 128, 32 },
+      text = "FIGHT >>>",
+      action = game.keyactions.a
+    },
+    -- unit2 attacks unit1
+    button:new {
+      pos = vec2:new { 512+16, 16 },
+      size = vec2:new { 128, 32 },
+      text = "<<< FIGHT",
+      action = game.keyactions.s
+    },
   }
 
 end
