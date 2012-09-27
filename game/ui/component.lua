d70478f452eec4760e26954509601c0e90517e61
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2012-09-27 15:37:41 -0300

    Beautifying code

--- game/ui/component.lua
+++ game/ui/component.lua
@@ -4,58 +4,54 @@ require "lux.table"
 
 require "vec2"
 
-module ("ui", package.seeall)
+local vec2    = vec2
+local object  = lux.object
 
-ui.component = lux.object.new {
-  pos = nil,
-  size = nil
-}
-
-local component = ui.component
-
-function component:__init ()
-  if not self.pos then
-    self.pos = vec2:new { 0, 0 }
+module "ui" do
+  
+  component = object.new {}
+  
+  component.__init = {
+    pos = vec2:new { 0, 0 },
+    size = vec2:new { 32, 32 }
+  }
+  
+  function component:left ()
+    return self.pos.x
   end
-  if not self.size then
-    self.size = vec2:new { 32, 32 }
+  
+  function component:top ()
+    return self.pos.y
   end
-end
-
-function component:left ()
-  return self.pos.x
-end
-
-function component:top ()
-  return self.pos.y
-end
-
-function component:right ()
-  return self.pos.x + self.size.x
-end
-
-function component:bottom ()
-  return self.pos.y + self.size.y
-end
-
-function component:inside (pos)
-  if pos.x < self:left() or
-     pos.y < self:top() or
-     pos.x > self:right() or
-     pos.y > self:bottom() then
-    return false
+  
+  function component:right ()
+    return self.pos.x + self.size.x
   end
-  return true
-end
-
-function component:pressed (b, pos)
-  -- Unimplemented component event.
-end
-
-function component:released (b, pos)
-  -- Unimplemented component event.
-end
-
-function component:draw ()
-  -- Unimplemented component event.
+  
+  function component:bottom ()
+    return self.pos.y + self.size.y
+  end
+  
+  function component:inside (pos)
+    if pos.x < self:left() or
+       pos.y < self:top() or
+       pos.x > self:right() or
+       pos.y > self:bottom() then
+      return false
+    end
+    return true
+  end
+  
+  function component:pressed (b, pos)
+    -- Unimplemented component event.
+  end
+  
+  function component:released (b, pos)
+    -- Unimplemented component event.
+  end
+  
+  function component:draw ()
+    -- Unimplemented component event.
+  end
+  
 end
