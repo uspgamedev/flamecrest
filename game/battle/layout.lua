13e3a11228eb004b070f4dee6ae904c2d7d8424a
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2012-10-05 16:34:04 -0300

    Tile map done.

--- game/battle/layout.lua
+++ game/battle/layout.lua
@@ -17,7 +17,7 @@ module "battle" do
   }
 
   function layout:load (graphics)
-    self.tileset.plains = graphics.newImage "resources/images/hextile.png"
+    self.tileset.plains = graphics.newImage "resources/images/hextile-border.png"
   end
   
   function layout:draw (graphics)
@@ -27,7 +27,7 @@ module "battle" do
       for j = 1,self.map.width do
         local tile  = self.map.tiles[i][j]
         if tile then
-          local pos   = vec2:new{96*i-96*j, 32*i+32*j}
+          local pos   = vec2:new{96*j-96*i, 32*j+32*i}
           local image = self.tileset[tile.type]
           if i == self.focus.i and j == self.focus.j then
             graphics.setColor(255,0,0,255)
@@ -46,8 +46,22 @@ module "battle" do
     if button == 'l' then
       local relpos = pos-self.origin
       relpos = relpos.x/192*vec2:new{1,-1} + relpos.y/64*vec2:new{1,1}
-      self.focus.i = floor(relpos.x+0.5)
-      self.focus.j = floor(relpos.y+0.5)
+      self.focus.i = floor(relpos.y+0.5)
+      self.focus.j = floor(relpos.x+0.5)
+      local x,y = relpos.x-self.focus.j+0.5, relpos.y-self.focus.i+0.5
+      if y > 2*x + 0.5 or y > x/2 + 0.75 then
+        if x + y < 1 then
+          self.focus.j = self.focus.j-1
+        else
+          self.focus.i = self.focus.i+1
+        end
+      elseif x > 2*y + 0.5 or x > y/2 + 0.75 then
+        if x + y < 1 then
+          self.focus.i = self.focus.i-1
+        else
+          self.focus.j = self.focus.j+1
+        end
+      end
     end
   end
 
