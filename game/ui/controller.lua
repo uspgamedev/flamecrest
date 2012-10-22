e5d4bac251482478dde672edbe120a4d963999f5
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2012-10-22 14:39:48 -0200

    Added ui.controller abstract class.

--- game/ui/controller.lua
+++ game/ui/controller.lua
@@ -8,10 +8,61 @@ local mouse = love.mouse
 
 module "ui" do
 
-  controller = object.new {}
+  controller = object.new {
+    layout = nil
+  }
+
+  controller.__init = {
+    keyactions = {
+      pressed   = {},
+      released  = {}
+    }
+  }
 
   function controller:update (dt)
-    -- Unimplemented cotroller update logic.
+    -- Unimplemented controller update logic.
+  end
+
+  function controller:mousepressed (button, pos)
+    -- Unimplemented controller mouse pressed event.
+  end
+
+  function controller:mousereleased (button, pos)
+    -- Unimplemented controller mouse released event.
+  end
+
+  function controller:presskey (key)
+    if self.keyactions.pressed[key] then
+      self.keyactions.pressed[key]()
+    end
+  end
+
+  function controller:releasekey (key)
+    if self.keyactions.released[key] then
+      self.keyactions.released[key]()
+    end
+  end
+
+  function controller:pressmouse (button, pos)
+    for i = #self.layout.components,1,-1 do
+      local component = self.layout.components[i]
+      if component.active and component:inside(pos) then
+        component.controller:pressmouse(button, pos)
+        return
+      end
+    end
+    self:mousepressed(button, pos)
+  end
+
+  function controller:releasemouse (button, pos)
+    for i = #self.layout.components,1,-1 do
+      local component = self.layout.components[i]
+      if component.active and component:inside(pos) then
+        component.contoller:releasemouse(button, pos)
+        return
+      end
+    end
+    self:mousereleased(button, pos)
   end
 
   function controller:mousepos ()
