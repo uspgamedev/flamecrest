8a59c748ccccd251efe3c4ae1702ee2ef61178c4
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2012-10-25 19:57:04 -0200

    Used safer game.changetolayout()

--- game/main.lua
+++ game/main.lua
@@ -39,7 +39,7 @@ function love.load ()
   battle.layout.map.tiles[5][9].unit = game.unit2
   game.layouts.battle = battle.layout
   game.layouts.combat = combat.layout
-  game.state.layout = combat.layout
+  game.changetolayout "combat"
 end
 
 love.update         = game.update
