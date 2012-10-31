e210bbb4dd42aeeb616cff482f2a4c74af800583
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2012-10-31 17:14:30 -0200

    Unit menu perceives mouse events

--- game/battle/menu/unit.lua
+++ game/battle/menu/unit.lua
@@ -1,16 +1,25 @@
 
 require "ui.component"
+require "ui.controller"
 require "vec2"
 
-local component = ui.component
-local vec2      = vec2
+local print       = print
+local component   = ui.component
+local controller  = ui.controller
+local vec2        = vec2
 
 module "battle.menu" do
 
   unit = component:new {
     active = false,
-    size = vec2:new{128, 256}
+    size = vec2:new{128, 256},
   }
+
+  unit:newcontroller()
+
+  function unit.controller:mousereleased (button, pos)
+    print "HEY"
+  end
 
   function unit:draw (graphics)
     graphics.setColor(25, 25, 25, 255)
