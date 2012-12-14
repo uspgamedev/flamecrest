b9f8811ded70402b3a27a99af2a9305e41111e72
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2012-12-13 23:48:20 -0200

    battle.component.mapintercace done.
    -Should I split it now?

--- game/battle/component/mapinterface.lua
+++ game/battle/component/mapinterface.lua
@@ -1,11 +1,15 @@
 
 require "vec2"
-require "battle.hexpos"
 require "ui.component"
+require "battle.hexpos"
+require "battle.controller"
+require "battle.menu.unitaction"
 
-local vec2   = vec2
-local ui     = ui
-local hexpos = battle.hexpos
+local vec2        = vec2
+local ui          = ui
+local hexpos      = battle.hexpos
+local unitmenu    = battle.menu.unitaction
+local controller  = battle.controller
 
 module "battle.component"
 
@@ -70,6 +74,17 @@ local function drawtile (mappos, image, context, graphics)
   end
 end
 
+local function drawunit (pos, tile, graphics)
+  if tile.unit and not tile.unit:isdead() then
+    graphics.draw(tile.unit.sprite, pos.x, pos.y, 0, 1, 1, 32, 85)
+  end
+end
+
+local function drawmodifier (name, pos, graphics)
+  local image = mapinterface.tileset[name]
+  graphics.draw(image, pos.x, pos.y, 0, 1, 1, 64, 35)
+end
+
 function mapinterface:do_draw (context, graphics)
   graphics.push()
   graphics.translate(context.layout.origin:get())
@@ -81,6 +96,17 @@ function mapinterface:do_draw (context, graphics)
         context,
         graphics
       )
+    end
+  )
+  if context.map.focus then
+    drawmodifier("focus", context.map.focus:tovec2(), graphics)
+  end
+  if not unitmenu.active then
+    drawmodifier("cursor", controller.cursor.pos:tovec2(), graphics)
+  end
+  context.map:pertile(
+    function (i, j, tile)
+      drawunit(hexpos:new{i,j}:tovec2(), tile, graphics)
     end
   )
   graphics.pop()
--- game/battle/layout.lua
+++ game/battle/layout.lua
@@ -26,23 +26,8 @@ module "battle" do
     self.map = map
     menu.unitaction.map = map
     component.mapinterface:load({map=map,layout=self}, graphics)
-    --function self.drawunitaction (i, j, tile)
-    --  self:drawunit(i,j,tile,graphics)
-    --end
     self:addcomponent(menu.unitaction)
   end
-
-  --function layout:drawunit (i, j, tile, graphics)
-  --  local pos = hexpos:new{i,j}:tovec2()
-  --  if tile.unit and not tile.unit:isdead() then
-  --    graphics.draw(tile.unit.sprite, pos.x, pos.y, 0, 1, 1, 32, 85)
-  --  end
-  --end
-
-  --function layout:drawmodifier (name, pos, graphics)
-  --  local image = self.tileset[name]
-  --  graphics.draw(image, pos.x, pos.y, 0, 1, 1, 64, 35)
-  --end
 
   function layout:update (dt)
     if self.map.focus then
@@ -60,27 +45,6 @@ module "battle" do
     end
   end
   
-  --function layout:draw (graphics)
-  --  graphics.push()
-  --  do
-  --    graphics.translate(self.origin:get())
-  --    self.map:pertile(self.drawtileaction)
-  --    if self.map.focus then
-  --      self:drawmodifier("focus", self.map.focus:tovec2(), graphics)
-  --    end
-  --    if not menu.unitaction.active then
-  --      self:drawmodifier("cursor", controller.cursor.pos:tovec2(), graphics)
-  --    end
-  --    self.map:pertile(self.drawunitaction)
-  --  end
-  --  graphics.pop()
-  --  do
-  --    local width = graphics.getFont():getWidth(self.map.mode)
-  --    graphics.print(self.map.mode, 512-width/2, 0)
-  --  end
-  --  ui.layout.draw(self, graphics)
-  --end
-
   function layout:focusedunit ()
     return self.map:focusedtile().unit
   end
