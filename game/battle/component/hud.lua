c26c60ed1358726bea14bd6b961a3b3f6848fc37
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2012-12-19 13:15:17 -0200

    [battle.component.hud] Refactored marker effect.

--- game/battle/component/hud.lua
+++ game/battle/component/hud.lua
@@ -5,6 +5,7 @@ require "battle.hexpos"
 require "battle.controller"
 require "battle.menu.unitaction"
 
+local string      = string
 local vec2        = vec2
 local ui          = ui
 local hexpos      = battle.hexpos
@@ -15,34 +16,34 @@ module "battle.component"
 
 hud = ui.component:new {
   pos     = vec2:new{0,0},
-  tileset = {}
+  spriteset = {}
 }
+
+local markereffectcode = [[
+  vec4 effect (vec4 color, Image texture, vec2 tex_pos, vec2 pix_pos) {
+    vec4 result = Texel(texture, tex_pos)*color;
+    number bright = 0.8+0.2*distance(tex_pos, vec2(0.5, 0.5));
+    return result.a*vec4(bright*%f, bright*%f, bright*%f, 0.6);
+  }
+]]
+
+local function makemarkereffect (graphics, r, g, b)
+  return graphics.newPixelEffect(string.format(markereffectcode, r, g, b))
+end
 
 function hud:load (context, graphics)
   self.size = vec2:new{graphics.getWidth(), graphics.getHeight()}
-  -- Load tileset images
-  self.tileset.marker = graphics.newImage "resources/images/hextile-empty.png"
-  self.tileset.focus  = graphics.newImage "resources/images/focus.png"
-  self.tileset.focus:setFilter("linear","linear")
-  self.tileset.cursor = graphics.newImage "resources/images/cursor.png"
-  self.tileset.cursor:setFilter("linear","linear")
+  -- Load spriteset images
+  self.spriteset.marker = graphics.newImage "resources/images/hextile-empty.png"
+  self.spriteset.focus  = graphics.newImage "resources/images/focus.png"
+  self.spriteset.focus:setFilter("linear","linear")
+  self.spriteset.cursor = graphics.newImage "resources/images/cursor.png"
+  self.spriteset.cursor:setFilter("linear","linear")
   function self:draw (graphics)
     self:do_draw(context, graphics)
   end
-  self.moveglow = graphics.newPixelEffect [[
-    vec4 effect (vec4 color, Image texture, vec2 tex_pos, vec2 pix_pos) {
-      vec4 result = Texel(texture, tex_pos)*color;
-      number bright = 0.8+0.2*distance(tex_pos, vec2(0.5, 0.5));
-      return result.a*vec4(0.0, 0.0, bright, 0.6);
-    }
-  ]]
-  self.atkglow = graphics.newPixelEffect [[
-    vec4 effect (vec4 color, Image texture, vec2 tex_pos, vec2 pix_pos) {
-      vec4 result = Texel(texture, tex_pos)*color;
-      number bright = 0.8+0.2*distance(tex_pos, vec2(0.5, 0.5));
-      return result.a*vec4(bright, bright/2.0, 0.0, 0.6);
-    }
-  ]]
+  self.moveglow = makemarkereffect(graphics, 0, 0, 1)
+  self.atkglow = makemarkereffect(graphics, 1, 0.5, 0)
 end
 
 local function drawmarker (mappos, context, graphics)
@@ -60,13 +61,13 @@ local function drawmarker (mappos, context, graphics)
     haseffect = true
   end
   if haseffect then
-    graphics.draw(hud.tileset.marker, pos.x, pos.y, 0, 1, 1, 64, 32)
+    graphics.draw(hud.spriteset.marker, pos.x, pos.y, 0, 1, 1, 64, 32)
     graphics.setPixelEffect()
   end
 end
 
 local function drawmodifier (name, pos, graphics)
-  local image = hud.tileset[name]
+  local image = hud.spriteset[name]
   graphics.draw(image, pos.x, pos.y, 0, 1, 1, 64, 35)
 end
 
