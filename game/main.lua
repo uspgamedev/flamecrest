6f53888f12aa3c4429186f1662e830a59932a7ef
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2013-01-03 01:00:38 -0200

    Removing combat layout stuff.

--- game/main.lua
+++ game/main.lua
@@ -1,6 +1,6 @@
 
 require "game"
-require "combat.layout"
+--require "combat.layout"
 require "vec2"
 require "battle.layout"
 require "battle.map"
@@ -8,15 +8,15 @@ require "battle.hexpos"
 
 math.randomseed( os.time() )
 
-local function loadcombatlayout ()
-  combat.layout:load(game)
-  combat.layout:addcomponent(
-    game.unit1:makedisplay(vec2:new{combat.layout.margin.left, combat.layout.margin.top})
-  )
-  combat.layout:addcomponent(
-    game.unit2:makedisplay(vec2:new{combat.layout.middle, combat.layout.margin.top})
-  )
-end
+--local function loadcombatlayout ()
+--  combat.layout:load(game)
+--  combat.layout:addcomponent(
+--    game.unit1:makedisplay(vec2:new{combat.layout.margin.left, combat.layout.margin.top})
+--  )
+--  combat.layout:addcomponent(
+--    game.unit2:makedisplay(vec2:new{combat.layout.middle, combat.layout.margin.top})
+--  )
+--end
 
 local function loadbattlemaplayout ()
   local map = battle.map:new { width = 9, height = 9 }
@@ -32,12 +32,12 @@ end
 function love.load ()
   love.graphics.setFont(love.graphics.newFont("fonts/Verdana.ttf", 14))
   love.graphics.setDefaultImageFilter("nearest", "nearest")
-  loadcombatlayout()
+  --loadcombatlayout()
   loadbattlemaplayout()
   battle.layout.map.tiles[5][1].unit = game.unit1
   battle.layout.map.tiles[5][9].unit = game.unit2
   game.layouts.battle = battle.layout
-  game.layouts.combat = combat.layout
+  --game.layouts.combat = combat.layout
   game.changetolayout "battle"
 end
 
