2d7dc919417b84bfd4b7dffc79b81ecea92b4c8c
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2013-03-14 09:34:22 -0300

    Removed mapscene state printing.

--- game/ui/battle/hud.lua
+++ game/ui/battle/hud.lua
@@ -75,5 +75,4 @@ function draw (map, mapscene, cursor, graphics)
   if not unitmenu.active then
     drawmodifier("cursor", cursor.pos():tovec2(), graphics)
   end
-  graphics.print(mapscene.mode, 0, 0)
 end
