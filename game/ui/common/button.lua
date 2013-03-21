34c1a14f4dab2939c89a02bf23b9896ef4040804
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2013-03-21 08:28:24 -0300

    Cleaned ui.common.button up.

--- game/ui/common/button.lua
+++ game/ui/common/button.lua
@@ -1,14 +1,14 @@
 
-require "ui.layout"
-require "ui.component"
-require "vec2"
-
-local vec2      = vec2
-local print     = print
-local module    = module
-local component = ui.component
-
 module "ui.common" do
+
+  require "ui.layout"
+  require "ui.component"
+  require "vec2"
+
+  local vec2      = vec2
+  local print     = print
+  local module    = module
+  local component = ui.component
 
   button = component:new {
     text = "<dummy text>",
