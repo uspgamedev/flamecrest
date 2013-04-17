b3af50f2fccbcb4cae33c97d0c324c64e4861abe
Renan <renanteruoc@gmail.com>
2013-04-17 18:57:24 -0300

    Changed tilelist to conform to new tile format

--- game/battle/tilelist.lua
+++ game/battle/tilelist.lua
@@ -5,20 +5,14 @@ module "battle" do
   
   tiletypes = {
     plains = object.new {
-      type = "plains",
-      bonus = {
-        avoid = 0,
-        def = 0,
-        mdef = 0
-      }
+      avoid = 0,
+      def = 0,
+      mdef = 0
     },
     forest = object.new {
-      type = "forest",
-      bonus = {
-        avoid = 10,
-        def = 1,
-        mdef = 0
-      }
+      avoid = 10,
+      def = 1,
+      mdef = 0
     }
   }
 
