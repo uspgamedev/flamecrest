ead1d3c0f7c54e6611a5da14fba5e1ad985ee913
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2014-11-09 15:50:59 -0200

    Moving old code, starting from "zero"

--- old/conf.lua
+++ old/conf.lua
@@ -1,7 +1,7 @@
 
 function love.conf(t)
-  t.identity = 'flamecrest'   -- The name of the save directory (string)
-  t.version = "0.9.0"         -- The LÃ–VE version the game was made for (string)
+  t.identity = nil            -- The name of the save directory (string)
+  t.version = "0.9.0"         -- The LÖVE version this game was made for (string)
   t.console = true            -- Attach a console (boolean, Windows only)
 
   t.author = "USPGameDev"     -- The author of the game (string)
@@ -20,7 +20,7 @@ function love.conf(t)
   t.window.fullscreentype = 'normal'
   t.window.vsync = true       -- Enable vertical sync (boolean)
   t.window.fsaa = 0           -- The number of FSAA-buffers (number)
-  t.window.display = 1
+  t.window.display = 2
 
   t.modules.audio = true      -- Enable the audio module (boolean)
   t.modules.event = true      -- Enable the event module (boolean)
