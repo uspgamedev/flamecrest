efed466a8520b35385d3cc5f3733c58321de55a0
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2015-01-14 19:02:01 -0200

    Adding attr setter to unit

--- game/domain/common/Unit.lua
+++ game/domain/common/Unit.lua
@@ -38,51 +38,93 @@ function common:Unit (name, class, basespec, growthspec)
     return maxhp
   end
 
+  function self:setMaxHP (val)
+    maxhp = val
+  end
+
   function self:getStr ()
     return str
+  end
+
+  function self:setStr (val)
+    str = val
   end
 
   function self:getMag ()
     return mag
   end
 
+  function self:setMag (val)
+    mag = val
+  end
+
   function self:getDef ()
     return def
+  end
+
+  function self:setDef (val)
+    def = val
   end
 
   function self:getRes ()
     return res
   end
 
+  function self:setRes (val)
+    res = val
+  end
+
   function self:getSpd ()
     return spd
+  end
+
+  function self:setSpd (val)
+    spd = val
   end
 
   function self:getSkl ()
     return skl
   end
 
+  function self:setSkl (val)
+    skl = val
+  end
+
   function self:getLck ()
     return lck
+  end
+
+  function self:setLck (val)
+    lck = val
   end
 
   function self:getMv ()
     return mv
   end
 
+  function self:setMv (val)
+    mv = val
+  end
+
   function self:getCon ()
     return con
+  end
+
+  function self:setCon (val)
+    con = val
   end
 
   function self:getAttr (attrname)
     local first = string.char(attrname:byte(1)-32)
     attrname = attrname:gsub("^(%a)", first)
+    attrname = attrname:gsub("hp", "HP")
     return self['get'..attrname](self)
   end
 
   function self:setAttr (attrname, value)
     local first = string.char(attrname:byte(1)-32)
     attrname = attrname:gsub("^(%a)", first)
+    attrname = attrname:gsub("hp", "HP")
     return self['set'..attrname](self, value)
   end
 
