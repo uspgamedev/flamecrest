1bf13c4fdd5b5500e985fb3c7aa471cee8ec02ee
Renan T. Carneiro <renantc@uspgamedev.org>
2015-01-14 19:05:03 -0200

    Unitspec now has a list of attributes

--- game/domain/common/unitspec.lua
+++ game/domain/common/unitspec.lua
@@ -1,6 +1,6 @@
 
 local unitspec = require 'lux.oo.prototype' :new {
-  maxhp = 20,
+--[[  maxhp = 20,
   str = 5,
   mag = 5,
   def = 5,
@@ -10,6 +10,28 @@ local unitspec = require 'lux.oo.prototype' :new {
   lck = 5,
   mv = 5,
   con = 7
+]]
 }
+
+local attrnames = {
+  'maxhp',
+  'str',
+  'mag',
+  'def',
+  'res',
+  'spd',
+  'skl',
+  'lck',
+  'mv',
+  'con',
+}
+
+for i, v in ipairs(attrnames) do
+  unitspec[v] = 5
+end
+
+function unitspec:attrNames()
+  return attrnames
+end
 
 return unitspec
