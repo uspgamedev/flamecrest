5c566e11809a23c5143a7cffb1cd59c3422c1235
Akira Mizutani <orenjiakira@gmail.com>
2015-01-21 22:24:29 -0200

    fixed most of conflicts of preview stats hud
    still bugged when using ranged weapons

--- game/activity/battle/PickTargetActivity.lua
+++ game/activity/battle/PickTargetActivity.lua
@@ -25,42 +25,37 @@ function battle:PickTargetActivity (UI, action)
   end
 
   function self.__accept:TileHovered (tile)
-    if not combat then
-      combat = action:startCombat(tile)
-      if combat then
-        local playerstats = {}
-        local foestats = {}
-        playerstats.hp, foestats.hp = combat:getHPs()
-        playerstats.dmg, foestats.dmg = combat:calculateDmgs()
-        playerstats.hit, foestats.hit = combat:calculateHits()
-        playerstats.crit, foestats.crit = combat:calculateCrits()
+    combat = action:startCombat(tile)
+    if combat then
+      local playerstats = {}
+      local foestats = {}
+      playerstats.hp, foestats.hp = combat:getHPs()
+      playerstats.dmg, foestats.dmg = combat:calculateDmgs()
+      playerstats.hit, foestats.hit = combat:calculateHits()
+      playerstats.crit, foestats.crit = combat:calculateCrits()
 
-        local playercombatwindow = ui.TextElement( "combatwindow", "", 14, vec2:new{20,20}, vec2:new{48,144} )
-        local foecombatwindow = ui.TextElement( "combatwindow", "", 14, vec2:new{88,20}, vec2:new{48,144} )
-        local playertext, foetext = "", ""
-        for k,v in pairs(playerstats) do
-          playertext = playertext .. k .. ": " .. playerstats[k] .. "\n"
-          foetext = foetext .. k .. ": " .. foestats[k] .. "\n"
-        end
-        playercombatwindow:setText(playertext)
-        foecombatwindow:setText(foetext)
-        UI:add(playercombatwindow)
-        UI:add(foecombatwindow)
-        UI:focus('screen')
-        return
-      else
-        return
+      local playercombatwindow = ui.TextElement( "combatwindow1", "", 14, vec2:new{20,20}, vec2:new{48,144} )
+      local foecombatwindow = ui.TextElement( "combatwindow2", "", 14, vec2:new{88,20}, vec2:new{48,144} )
+      local playertext, foetext = "", ""
+      for k,v in pairs(playerstats) do
+        playertext = playertext .. k .. ": " .. playerstats[k] .. "\n"
+        foetext = foetext .. k .. ": " .. foestats[k] .. "\n"
       end
+      playercombatwindow:setText(playertext)
+      foecombatwindow:setText(foetext)
+      UI:add(playercombatwindow)
+      UI:add(foecombatwindow)
+      UI:focus('screen')
+    else
+      UI:remove('combatwindow1')
+      UI:remove('combatwindow2')
     end
   end
 
   function self.__accept:TileClicked (tile)
-    if not combat then
-      combat = action:startCombat(tile)
-      if combat then
-        UI:find("screen"):clearRange()
-        self:addTask('CombatAnimation', combat)
-      end
+    if combat then
+      UI:find("screen"):clearRange()
+      self:addTask('CombatAnimation', combat)
     end
   end
 
